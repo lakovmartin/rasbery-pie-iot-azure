203837bb517c4c1647b310fe4ab9021479baeed1
Martin <40353389+lakovmartin@users.noreply.github.com>
2023-04-04 17:08:02 +1000

    added docker group

--- rasbery-installs/install-docker-rasbian.sh
+++ rasbery-installs/install-docker-rasbian.sh
@@ -24,6 +24,9 @@ sudo groupadd docker
 # Add user to docker group
 sudo usermod -aG docker $USER
 
+# Activate Changes to Group
+newgrp docker
+
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
