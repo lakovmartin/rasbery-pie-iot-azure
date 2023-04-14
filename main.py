31981ea59f94c0170ba34491a335ee3117b061e3
Martin <40353389+lakovmartin@users.noreply.github.com>
2023-04-14 23:27:04 +1000

    new script

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
-import os
-import sys
-import time
-from azure.iot.device import IoTHubDeviceClient, Message
+import random  
 import Adafruit_DHT
+import time
+from azure.iot.device import IoTHubDeviceClient, Message  
 
-# Read the IoT Hub device connection string from an environment variable.
-CONNECTION_STRING = os.environ["CONNECTION_STRING"]
+sensor = Adafruit_DHT.DHT22
+pin = 21
 
-# Set the DHT22 sensor pin number.
-SENSOR_PIN = 4
+CONNECTION_STRING = "HostName=xxxxxxxx;DeviceId=xxxxxx;SharedAccessKey=xxxxxxxxxxxxxxxxxxx"  
 
-# Set the time interval in seconds for sending data to Azure IoT Hub.
-SEND_INTERVAL = 30
-
-# Set the message format for sending data to Azure IoT Hub.
-MESSAGE_FORMAT = '{{"temperature":{0:0.1f},"humidity":{1:0.1f}}}'
-
-def read_sensor_data():
-    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, SENSOR_PIN)
-    return temperature, humidity
-
-def send_data_to_iothub(device_client, message):
-    try:
-        device_client.send_message(message)
-        print("Message sent to Azure IoT Hub: {}".format(message))
-    except Exception as e:
-        print("Error sending message to Azure IoT Hub: {}".format(e))
-
-if __name__ == "__main__":
-    try:
-        # Initialize the Azure IoT Hub device client.
-        client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-
-        while True:
-            temperature, humidity = read_sensor_data()
-            message = MESSAGE_FORMAT.format(temperature, humidity)
-            send_data_to_iothub(client, message)
-            time.sleep(SEND_INTERVAL)
-    except KeyboardInterrupt:
-        print("Stopped by the user")
-    except Exception as e:
-        print("Error: {}".format(str(e)))
+MSG_SND = '{{"temperature": {temperature},"humidity": {humidity}}}'  
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    def iothub_client_init():  
+        client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)  
+        return client  
+    def iothub_client_telemetry_sample_run():  
+        try:  
+            client = iothub_client_init()  
+            print ( "Sending data to IoT Hub, press Ctrl-C to exit" )  
+            while True:  
+                msg_txt_formatted = MSG_SND.format(temperature=temperature, humidity=humidity)  
+                message = Message(msg_txt_formatted)  
+                print( "Sending message: {}".format(message) )  
+                client.send_message(message)  
+                print ( "Message successfully sent" )  
+                time.sleep(3)  
+        except KeyboardInterrupt:  
+            print ( "IoTHubClient stopped" )  
+    if __name__ == '__main__':  
+        print ( "Press Ctrl-C to exit" )  
+        iothub_client_telemetry_sample_run()
